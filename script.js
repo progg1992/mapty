ae6cd318cc9782e8d39ae436448cd8f84728bcfd
Pierce Rogg <roggpierce@gmail.com>
2020-11-11 14:04:25 -0600

    fixed repeating code

--- script.js
+++ script.js
@@ -25,21 +25,11 @@ navigator.geolocation.getCurrentPosition(function(position) {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    map.on('click', function(mapEvent) {
-        const {lat, lng} = mapEvent.latlng;
-
-        L.marker([lat, lng])
-            .addTo(map)
-            .bindPopup(L.popup({
-                maxWidth: 250,
-                minWidth: 100,
-                autoClose: false,
-                closeOnClick: false,
-                className: 'running-popup',
-            }))
-            .setPopupContent('Workout')
-            .openPopup();
-    })    
+    map.on('click', function(mapE) {
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+    })        
 
 }, function() {
     alert('Could not get your position')
@@ -49,7 +39,7 @@ form.addEventListener('submit', function(e) {
     e.preventDefault();
 
     inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
-    // Display marker
+    
     const {lat, lng} = mapEvent.latlng;
     L.marker([lat, lng])
         .addTo(map)
